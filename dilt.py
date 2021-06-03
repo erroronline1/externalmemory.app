1f4f92984ed6e9f6382357042ac4abdb9e97f90f
erroronline1 <dev@erroronline.one>
2021-06-03 15:58:26 +0200

    added (temporary) storing and remembering

--- dilt.py
+++ dilt.py
@@ -11,6 +11,7 @@ from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty, StringProperty, BooleanProperty, Property
+import json
 import cv2
 from pyzbar import pyzbar
 
@@ -29,11 +30,13 @@ class Screen(Widget):
 	def content(self, element, **kwargs):
 		if len(kwargs):
 			for arg in kwargs:
-				# conditional property check because dynamic assignment subscritaple type error
+				# conditional property check because dynamic assignment throws an unsubscriptable type error
 				if arg=='text':
 					self.ids[element].text = kwargs[arg]
 				elif arg=='texture':
 					self.ids[element].texture = kwargs[arg]
+				elif arg=='state':
+					self.ids[element].state = kwargs[arg]
 		lang = "en"
 		return language(element, lang)
 	
@@ -45,15 +48,17 @@ class Screen(Widget):
 			if el.state == "down":
 				rating = states.index(el)
 		################ create rating summary
-		self.productrating = {
+		DiltApp.store(DiltApp, {
 			"code" : self.ids.detectedcode.text,
 			"description" : self.ids.mynotes.text,
 			"rating" : rating
-		}
-		print (self.productrating)
+		})
+		self.content('mydata', text=json.dumps(DiltApp.productrating, indent=4))
 
 
 class DiltApp(App): # <- Main Class
+	productrating=[]
+
 	def build(self):
 		self.capture = cv2.VideoCapture(0)
 		Clock.schedule_interval(self.camupdate, 1.0 / 60)
@@ -80,8 +85,39 @@ class DiltApp(App): # <- Main Class
 			barcode_info = barcode.data.decode('utf-8')
 			cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
 			############### pass result to label
-			self.Screen.content('detectedcode', text = barcode.type + " | " + barcode_info)
+			detected=barcode.type + " | " + barcode_info
+			self.lookup(detected)
+			self.Screen.content('detectedcode', text = detected)
 		return frame
+
+	def store(self, values):
+		known = False
+		for i, el in enumerate(self.productrating):
+			if self.productrating[i]['code'] == values['code']:
+				self.productrating[i]['description'] = values['description']
+				self.productrating[i]['rating'] = values['rating']
+				known = True
+		if not known:
+			self.productrating.append(values)
+
+	def lookup(self, id):
+		mynotes=''
+		good= False
+		meh=False
+		bad = False
+		for i, el in enumerate(self.productrating):
+			if self.productrating[i]['code'] == id:
+				mynotes = self.productrating[i]['description']
+				if self.productrating[i]['rating'] == 2:
+					good = True
+				if self.productrating[i]['rating'] == 1:
+					meh = True
+				if self.productrating[i]['rating'] == 0:
+					bad = True
+		self.Screen.content('mynotes', text = mynotes)
+		self.Screen.content('good', state = 'down' if good else 'normal')
+		self.Screen.content('meh', state = 'down' if meh else 'normal')
+		self.Screen.content('bad', state = 'down' if bad else 'normal')
 
 	def on_stop(self):
 		#without this, app will not exit even if the window is closed
